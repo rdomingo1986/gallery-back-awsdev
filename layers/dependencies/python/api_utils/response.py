587eaa746a6f37763fec5998732669df54f96c85
drmingus86 <dr_mingo86@hotmail.com>
2025-08-12 01:43:18 -0500

    refactorización del layer para eliminar comentarios y enviar solo Methods necesarios en los CORS

--- layers/dependencies/python/api_utils/response.py
+++ layers/dependencies/python/api_utils/response.py
@@ -1,22 +1,14 @@
 import json
 
-def api_response(status_code, message):
-    """
-    Genera una respuesta API estándar con headers CORS
+def api_response(status_code, data):
+    body = {"message": data} if isinstance(data, str) else data
     
-    Args:
-        status_code (int): Código de estado HTTP
-        message (str): Mensaje de respuesta
-    
-    Returns:
-        dict: Respuesta formateada para API Gateway
-    """
     return {
         "statusCode": status_code,
         "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "DELETE,OPTIONS",
+            "Access-Control-Allow-Origin": "'*'",
+            "Access-Control-Allow-Methods": "'GET,PUT,DELETE,OPTIONS'",
             "Access-Control-Allow-Headers": "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Requested-With'"
         },
-        "body": json.dumps({"message": message})
+        "body": json.dumps(body)
     }
